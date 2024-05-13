3fba8ca8b59f9cd4a460691960d59d7da0922527
adityahongal <adityahongalwork@gmail.com>
2024-05-13 20:07:36 +0530

    Variables & Datatypes in CPP

---
+++ cpp-basics/VariablesAndDatatypes/Datatypes.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int
+    int count = 5;
+
+    // float
+    float share = 3.77;
+
+    // char datatype
+    char alphabet = 'z';
+
+    // Double
+    double weight = 55.908765;
+
+    // Boolean
+    bool isMale = true;
+    bool ischild= 1;
+    bool isFemale = 0;
+
+    cout << "count: " << count << endl;
+    cout << "share: " << share << endl;
+    cout << "alphabet: " << alphabet << endl;
+    cout << "weight: " << weight << endl;
+    cout << "isMale: " << isMale << endl;
+    return 0;
+}
+
+
+// These are primitive/builtin datatypes
---
+++ cpp-basics/VariablesAndDatatypes/Variables.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Variable define
+    // int age = 25;
+    // cout << "My age is :" << age << endl;
+
+    // Declaration
+    // int age;
+    // cout << age << endl;
+
+    // Definition
+    int age = 26;
+    cout << age << endl;
+
+    // Manipulation/Updation
+    age = 50;
+    cout << age << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+// int is datatype i.e. integer
+// age is variable name which stores the value in the memory block
+// 25 is the value
+
+// When we use Declaration without value it stores some garbage value and returns it.
+
+// When we use Definition we declare a variable and its value
