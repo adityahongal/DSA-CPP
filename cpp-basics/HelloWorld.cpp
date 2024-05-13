3a3239f8fc690542445ae6a6cbd1d627ed3d3d52
adityahongal <adityahongalwork@gmail.com>
2024-05-13 19:22:46 +0530

    Destructuring HelloWorld program

--- cpp-basics/HelloWorld.cpp
+++ cpp-basics/HelloWorld.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+int main(){
+
+  cout<<"Hello world!"<<endl;
   return 0;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// In C++ int main(), for the example above is the main() function. anything inside "{ }" is considered as the properties of that function which defines scope of that function.
+// If asked define scope then whatever inside "{ }" is considered as the properties/scope of that function.
+// int main() is the starting point of our code
+
+// return 0 - represents succesful execution of our code
+// If it returns non zero number for example 12,89,etc then it represents unsuccessful execution
+
+// Here <iostream> is a header file which consists of functionality related to inputs and outputs and standard library related.
+
+// using namespace std; ----- defines the standard namespace. It consists of definition for identifiers such as "cout" and "endl".
+// If we want to use "cout" we need to define the namespace library and its related functionality using iostream and namespace std.
+
+// cout ------ is a identifier used to get/print output
+// << --> We use this insertion operator with cout to print something
+
+// "Hello, world!" --- Anythng inside " " is called string.
+
+// endl-----is used to print on a new line
+
+// ; --- is used for termination of that line
+
