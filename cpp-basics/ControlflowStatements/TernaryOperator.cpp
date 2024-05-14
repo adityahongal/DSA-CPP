982a9158a6c9d3f71cac1cbe9b48b126e26ceb71
adityahongal <adityahongalwork@gmail.com>
2024-05-14 12:28:00 +0530

    Ternary Operator

---
+++ cpp-basics/ControlflowStatements/TernaryOperator.cpp
@@ -0,0 +1,16 @@
+// Ternary Operator syntax
+
+// condition ? expression_is_true : expression_is_false ;
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int age = 12;
+
+    (age > 18) ? cout << "You can vote" << endl : cout << "You cannot vote" << endl ;
+
+    return 0;
+}
