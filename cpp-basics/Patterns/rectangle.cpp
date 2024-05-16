9a68928a8fcb2db6fe58ae321769a7b14226296c
adityahongal <adityahongalwork@gmail.com>
2024-05-16 14:02:38 +0530

    Custom rectangle

--- cpp-basics/Patterns/rectangle.cpp
+++ cpp-basics/Patterns/rectangle.cpp
@@ -3,20 +3,42 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+// int main(){
 
-    // total rows = 3
-    // total columns = 5
-    // every row contains -> 5 stars
+//     // total rows = 3
+//     // total columns = 5
+//     // every row contains -> 5 stars
 
-    // row -> outer loop 
-    // column -> inner loop 
+//     // row -> outer loop 
+//     // column -> inner loop 
 
-    for(int row = 0; row < 3; row++){
-        for(int col = 0; col < 5; col++){
+//     for(int row = 0; row < 3; row++){
+//         for(int col = 0; col < 5; col++){
+//             cout << "*" << " ";
+//         }
+//         // print on newline after every row
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// CUSTOM RECANTANGLES WITH USER INPUTS
+
+int main (){
+
+    int length;
+    int width;
+
+    cout << "Enter length: ";
+    cin >> length;
+
+    cout << "Enter width: ";
+    cin >> width;
+
+    for (int row=0; row < length; row++){
+        for (int col = 0; col < width; col++){
             cout << "*" << " ";
         }
-        // print on newline after every row
         cout << endl;
     }
     return 0;
