cb4d5a356de2db685407ad12c02c897ad99de6c0
adityahongal <adityahongalwork@gmail.com>
2024-05-17 13:42:47 +0530

    Half Pyramid

--- cpp-basics/Patterns/halfPyramid.cpp
+++ cpp-basics/Patterns/halfPyramid.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main() {
 
-    
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    for ( int row = 0; row < n; row++){
+        for ( int col = 0; col < row+1; col++){                        // alternatively can use col <= row; cz both are same
+            cout << "* ";
+        }
+        cout << endl;
+    }
     return 0;
 }
