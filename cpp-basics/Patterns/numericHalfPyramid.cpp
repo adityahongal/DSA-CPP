3a20e3110d7be140801f6e7c0630ec4bb4678353
adityahongal <adityahongalwork@gmail.com>
2024-05-17 14:45:13 +0530

    NUMERIC INVERTED HALF PYRAMID

---
+++ cpp-basics/Patterns/numericHalfPyramid.cpp
@@ -0,0 +1,19 @@
+// NUMERIC HALF PYRAMID
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    for ( int row = 0; row < n; row++){
+        for ( int col = 0; col < row+1; col++){                        
+            cout << (col +1) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ cpp-basics/Patterns/numericInvertedHalfPyramid.cpp
@@ -0,0 +1,19 @@
+// NUMERIC INVERTED HALF PYRAMID
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    for ( int row = 0; row < n; row++){
+        for ( int col = 0; col < n-row ; col++){                       
+            cout << (col+1) <<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
