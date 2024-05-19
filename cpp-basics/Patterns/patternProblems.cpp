dd7bbe9e03afc702974c2c14fb31b5b9181821f9
adityahongal <adityahongalwork@gmail.com>
2024-05-19 21:04:34 +0530

    numeric Palindrome Equilateral Pyramid

--- cpp-basics/Patterns/patternProblems.cpp
+++ cpp-basics/Patterns/patternProblems.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
 
-// void numericHollowHalfPyramid(int n){
-
+// void numericHollowHalfPyramid(){
+//     int n;
+//     cout << "Enter the value of n: ";
+//     cin >> n;
 //     for ( int row = 0; row < n; row++ ){
 //         for ( int col = 0; col <= row; col++){
 //             if ( col == 0 || col == row || row == n - 1 )
@@ -14,28 +16,53 @@ using namespace std;
 //     }
 // }
 
-void numericHollowInvertedHalfPyramid(int n){
+// void numericHollowInvertedHalfPyramid(){
+//     int n;
+//     cout << "Enter the value of n: ";
+//     cin >> n;
+//     for ( int row = 0; row < n; row++){
+//         for ( int col = row + 1; col <= n; col++){
+//             // cout << col;
+//             if ( col == row + 1 || col == n || row == 0)
+//             cout << col;
+//             else
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
 
-    for ( int row = 0; row < n; row++){
-        for ( int col = row + 1; col <= n; col++){
-            // cout << col;
-            if ( col == row + 1 || col == n || row == 0)
-            cout << col;
-            else
+void numericPalindromeEquilateralPyramid(){
+    int size;
+    cout << "Enter Size of pattern" << endl;
+    cin >> size;
+    
+    for (int i = 0; i < size; i++) {
+        // Print leading spaces
+        for (int j = 0; j < size - i - 1; j++) {
             cout << " ";
         }
+        
+        // Print ascending numbers
+        for (int j = 0; j <= i; j++) {
+            cout << j + 1;
+        }
+
+        // Print descending numbers
+        for (int j = i - 1; j >= 0; j--) {
+            cout << j + 1;
+        }
+
+        // Move to the next line
         cout << endl;
     }
 }
 
 int main(){
 
-    int n;
-    cout << "Enter the value of n: ";
-    cin >> n;
-
-    // numericHollowHalfPyramid(n);
-    numericHollowInvertedHalfPyramid(n);
+    // numericHollowHalfPyramid();
+    // numericHollowInvertedHalfPyramid();
+    numericPalindromeEquilateralPyramid();
 
     return 0;
 }
