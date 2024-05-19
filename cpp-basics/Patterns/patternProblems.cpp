4dbb17bdd11b904284b491fcb6e63b574f184119
adityahongal <adityahongalwork@gmail.com>
2024-05-19 22:34:02 +0530

    Solid half Diamond

--- cpp-basics/Patterns/patternProblems.cpp
+++ cpp-basics/Patterns/patternProblems.cpp
@@ -32,28 +32,50 @@ using namespace std;
 //     }
 // }
 
-void numericPalindromeEquilateralPyramid(){
-    int size;
-    cout << "Enter Size of pattern" << endl;
-    cin >> size;
+// void numericPalindromeEquilateralPyramid(){
+//     int size;
+//     cout << "Enter Size of pattern" << endl;
+//     cin >> size;
     
-    for (int i = 0; i < size; i++) {
-        // Print leading spaces
-        for (int j = 0; j < size - i - 1; j++) {
-            cout << " ";
-        }
+//     for (int i = 0; i < size; i++) {
+//         // Print leading spaces
+//         for (int j = 0; j < size - i - 1; j++) {
+//             cout << " ";
+//         }
         
-        // Print ascending numbers
-        for (int j = 0; j <= i; j++) {
-            cout << j + 1;
-        }
+//         // Print ascending numbers
+//         for (int j = 0; j <= i; j++) {
+//             cout << j + 1;
+//         }
 
-        // Print descending numbers
-        for (int j = i - 1; j >= 0; j--) {
-            cout << j + 1;
-        }
+//         // Print descending numbers
+//         for (int j = i - 1; j >= 0; j--) {
+//             cout << j + 1;
+//         }
 
-        // Move to the next line
+//         // Move to the next line
+//         cout << endl;
+//     }
+// }
+
+void solidHalfDiamond(){
+    int n;
+    cout << "Enter Size of pattern:" << endl;
+    cin >> n;
+
+    // To print stars in ascending order
+    for ( int row = 0; row < n; row++){
+        for ( int col = 0; col < row + 1; col++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // Then Print stars in descending order
+    for ( int row = 0; row < n - 1; row++ ){
+        for ( int col = 0; col < n - row - 1; col++){
+            cout << "* ";
+        }
         cout << endl;
     }
 }
@@ -62,7 +84,8 @@ int main(){
 
     // numericHollowHalfPyramid();
     // numericHollowInvertedHalfPyramid();
-    numericPalindromeEquilateralPyramid();
+    // numericPalindromeEquilateralPyramid();
+    solidHalfDiamond();
 
     return 0;
 }
