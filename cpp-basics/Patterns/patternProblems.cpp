7fa11701e0971edc834748a972aba226f955a0c2
adityahongal <adityahongalwork@gmail.com>
2024-05-19 19:49:30 +0530

    Numeric Hollow Inverted Half Pyramid

--- cpp-basics/Patterns/patternProblems.cpp
+++ cpp-basics/Patterns/patternProblems.cpp
@@ -1,14 +1,28 @@
 #include<iostream>
 using namespace std;
 
-void numericHollowHalfPyramid(int n){
+// void numericHollowHalfPyramid(int n){
 
-    for ( int row = 0; row < n; row++ ){
-        for ( int col = 0; col <= row; col++){
-            if ( col == 0 || col == row || row == n - 1 )
-                cout << col + 1;
+//     for ( int row = 0; row < n; row++ ){
+//         for ( int col = 0; col <= row; col++){
+//             if ( col == 0 || col == row || row == n - 1 )
+//                 cout << col + 1;
+//             else
+//                 cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+void numericHollowInvertedHalfPyramid(int n){
+
+    for ( int row = 0; row < n; row++){
+        for ( int col = row + 1; col <= n; col++){
+            // cout << col;
+            if ( col == row + 1 || col == n || row == 0)
+            cout << col;
             else
-                cout << " ";
+            cout << " ";
         }
         cout << endl;
     }
@@ -20,7 +34,8 @@ int main(){
     cout << "Enter the value of n: ";
     cin >> n;
 
-    numericHollowHalfPyramid(n);
+    // numericHollowHalfPyramid(n);
+    numericHollowInvertedHalfPyramid(n);
 
     return 0;
 }
