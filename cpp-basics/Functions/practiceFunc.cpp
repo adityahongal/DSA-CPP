68d02504b00a4ac30192ec571a0c8a9a9d0323c5
adityahongal <adityahongalwork@gmail.com>
2024-05-21 21:16:43 +0530

    printing all integers

--- cpp-basics/Functions/practiceFunc.cpp
+++ cpp-basics/Functions/practiceFunc.cpp
@@ -2,18 +2,30 @@
 using namespace std;
 
 // Kilometer to Miles
-void KMtoMiles(){
-    float km;
-    cout << "Enter Kilometer: ";
-    cin >> km;
-    float _1km = 0.621371;
-    float miles = km * _1km;
-    cout << km << " Kilometer = " << miles << " Miles" << endl;
+// void KMtoMiles(){
+//     float km;
+//     cout << "Enter Kilometer: ";
+//     cin >> km;
+//     float _1km = 0.621371;
+//     float miles = km * _1km;
+//     cout << km << " Kilometer = " << miles << " Miles" << endl;
+// }
+
+void printAllDigits(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    while(n>0){
+        int onesPlaceDigit = n % 10;
+        cout << "Digit is : " << onesPlaceDigit << endl;
+        n = n / 10;
+    }
 }
 
 int main(){
 
-    KMtoMiles();
-    
+    // KMtoMiles();
+    printAllDigits();
+
     return 0;
 }
