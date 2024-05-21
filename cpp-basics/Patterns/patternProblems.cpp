f3a07985b65e0aebc482f9b601119b4a181d0a53
adityahongal <adityahongalwork@gmail.com>
2024-05-21 15:17:51 +0530

    Floyds Triangle

--- cpp-basics/Patterns/patternProblems.cpp
+++ cpp-basics/Patterns/patternProblems.cpp
@@ -149,24 +149,38 @@ using namespace std;
 
 // }
 
-void fancyPatternThree(){
-    int n;
-    cout << "Enter the size of pattern:" << endl;
-    cin >> n;
+// void fancyPatternThree(){
+//     int n;
+//     cout << "Enter the size of pattern:" << endl;
+//     cin >> n;
 
-    for (int row = 0; row < n; row++) {
-        int cond = row <= n/2 ? 2 * row : 2 * ( n - row - 1 );
-        for ( int col = 0; col <= cond; col++){
-            if ( col <= cond/2 ){
-                cout << col + 1;
-            }
-            else {
-                cout << cond - col + 1;
-            }
-        }
-        cout << endl;
-    }
+//     for (int row = 0; row < n; row++) {
+//         int cond = row <= n/2 ? 2 * row : 2 * ( n - row - 1 );
+//         for ( int col = 0; col <= cond; col++){
+//             if ( col <= cond/2 ){
+//                 cout << col + 1;
+//             }
+//             else {
+//                 cout << cond - col + 1;
+//             }
+//         }
+//         cout << endl;
+//     }
     
+// }
+
+void floydsTriangle() {
+  int size;
+  cout << "Enter Size of pattern" << endl;
+  cin >> size;
+  int c = 1;
+  for (int row = 0; row < size; row++) {
+    for (int col = 0; col <= row ; col++) {
+      cout << c << "  ";
+      c++;
+    }
+    cout << endl;
+  }
 }
 
 int main(){
@@ -177,7 +191,8 @@ int main(){
     // solidHalfDiamond();
     // fancyPatternOne();
     // fancyPatternTwo();
-    fancyPatternThree();
+    // fancyPatternThree();
+    floydsTriangle();
 
     return 0;
 }
