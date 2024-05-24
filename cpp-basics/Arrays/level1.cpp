f90c91e1489deb59102651705066b4017256a300
adityahongal <adityahongalwork@gmail.com>
2024-05-24 14:25:27 +0530

    Creating an ARRAY

---
+++ cpp-basics/Arrays/level1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    // Creating an Array
+    int arr[10];
+
+    // int brr[];         // Throws an error because size of array is not defined
+
+    // Initializing an empty array
+    int crr[] = {10,20,80,70,50};
+
+    int drr[4] = {55};
+
+    cout << crr[3] << endl;
+    cout << drr[0] << endl;
+    cout << drr[1] << endl;
+    
+    return 0;
+}
