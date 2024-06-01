5e229b63f92d47dac4b0cd897ffd36d8f60ad5c6
adityahongal <adityahongalwork@gmail.com>
2024-06-01 12:19:01 +0530

    Vector Intro

--- cpp-basics/Vectors/vectorsIntro.cpp
+++ cpp-basics/Vectors/vectorsIntro.cpp
@@ -2,38 +2,63 @@
 #include <vector> // needs header
 using namespace std;
 
-void printArray(int a[], int size) {
-  for (int i = 0; i < size; i++) {
-    cout << a[i] << endl;
-  }
+// void printArray(int a[], int size) {
+//   for (int i = 0; i < size; i++) {
+//     cout << a[i] << endl;
+//   }
+// }
+
+// void basicArrayOps() {
+//   // static memory allocation of an array
+//   int arr[5] = {1, 2, 4, 5, 7};
+//   printArray(arr, 5);
+
+//   // dynamic memory allocation but not shrink
+
+//   int size;
+//   cout << "enter size of dynamic array" << endl;
+//   cin >> size;
+//   int *brr = new int[size];
+//   // the value stored will be either 0 or garbage if
+//   // not entered any
+//   // can not add more elements then specified size taken from user
+
+//   // To add user input to the array
+//   cout << "Enter your input" << endl;
+//   for (int i = 0; i < size; i++) {
+//     int data;
+//     cin >> data;
+//     brr[i] = data;
+//   }
+//   printArray(brr, size);
+// }
+
+void printVectors(vector<int> v){
+    // to know size of vectors
+    int size = v.size();
+
+    // Print the vectors
+    for (int i = 0; i < size; i++){
+        cout << v[i] << endl;
+    }
 }
 
-void basicArrayOps() {
-  // static memory allocation of an array
-  int arr[5] = {1, 2, 4, 5, 7};
-  printArray(arr, 5);
+void vectors(){
+    // initialize a vector
+    vector<int> v;
 
-  // dynamic memory allocation but not shrink
-  int size;
-  cout << "enter size of dynamic array" << endl;
-  cin >> size;
-  int *brr = new int[size];
-  // the value stored will be either 0 or garbage if
-  // not entered any
-  // can not add more elements then specified size taken from user
+    // Insert elements into vector
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
 
-  // To add user input to the array
-  cout << "Enter your input" << endl;
-  for (int i = 0; i < size; i++) {
-    int data;
-    cin >> data;
-    brr[i] = data;
-  }
-  printArray(brr, size);
+    printVectors(v);
+    
 }
 
 int main(){
 
-    basicArrayOps();
+    // basicArrayOps();
+    vectors();
     return 0;
 }
