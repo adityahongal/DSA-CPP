e5ddf68194990b380148cc275c4eb584a06c1e43
adityahongal <adityahongalwork@gmail.com>
2024-06-02 14:23:09 +0530

    taking input in 2D array

--- cpp-basics/Arrays/level3.cpp
+++ cpp-basics/Arrays/level3.cpp
@@ -4,15 +4,15 @@
 #include <vector>
 using namespace std;
 
-// void print2Darray(int arr[][2],int row,int col){        //In C++, when you pass a 2D array to a function, you need to specify the size of the second dimension explicitly
-//     //Row wise traversal
-//     for(int i=0;i<row;i++){
-//         for(int j=0;j<col;j++){
-//             cout << arr[i][j] << " ";
-//         }
-//         cout << endl;
-//     }
-// }
+void print2DArray(int drr[][4],int row,int col){        //In C++, when you pass a 2D array to a function, you need to specify the size of the second dimension explicitly
+    //Row wise traversal
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout << drr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 // void print2DArrayColWise(int brr[][4], int rows, int cols) {
 //   cout << "Printing Array" << endl;
@@ -25,24 +25,24 @@ using namespace std;
 //   }
 // }
 
-void print2DArrayDiagonalwise(int crr[][4], int rows, int cols) {
-  cout << "Printing Array" << endl;
+// void print2DArrayDiagonalwise(int crr[][4], int rows, int cols) {
+//   cout << "Printing Array" << endl;
 
-  // Method 1
+//   // Method 1
+// //   for(int i=0; i < rows; i++) {
+// //     for(int j=0; j<cols;j++){
+// //         if(i==j){
+// //             cout << crr[i][j] << " ";
+// //         }
+// //     }
+// //     cout << endl;
+// //   }
+
+//   // Method 2
 //   for(int i=0; i < rows; i++) {
-//     for(int j=0; j<cols;j++){
-//         if(i==j){
-//             cout << crr[i][j] << " ";
-//         }
-//     }
-//     cout << endl;
+//     cout << crr[i][i] << " ";
 //   }
-
-  // Method 2
-  for(int i=0; i < rows; i++) {
-    cout << crr[i][i] << " ";
-  }
-}
+// }
 
 void example1(){
     // Intializing 2D array
@@ -62,9 +62,24 @@ void example1(){
     // Can be done only in square matrix format
     int crr[4][4] = {{10, 20, 30, 40}, {50, 60, 70, 80}, {500, 600, 700, 800}, {699,799,899,999}};
 
-    // print2Darray( arr, 3, 2);
+    // Taking input for 2D array - row wise
+    int drr[3][4];
+
+    int rows = 3;
+    int cols = 4;
+    cout << "Enter elements for array input" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << "Enter Element for row: " << i << " and col: " << j << endl;
+            cin >> drr[i][j];
+        }
+     }
+
+    print2DArray(drr, 3, 4);
+
+    // print2DArray( arr, 3, 2);
     // print2DArrayColWise(brr, 3, 4);
-    print2DArrayDiagonalwise(crr, 4, 4);
+    // print2DArrayDiagonalwise(crr, 4, 4);
 
 }
 
