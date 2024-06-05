d7b60b6e2c2111039e99e17279e0a8963e96f9d2
adityahongal <adityahongalwork@gmail.com>
2024-06-05 14:18:33 +0530

    Finding Pivot Index

--- cpp-basics/Arrays/arrayAssignment.cpp
+++ cpp-basics/Arrays/arrayAssignment.cpp
@@ -36,10 +36,62 @@ bool haskeyPairTwoSum(){
     return false;
 }
 
+void findPivotIndex(){
+    // vector creation
+    vector<int> nums;
 
+    // vector insertion
+    // case 1 ans 3
+    nums.push_back(1); // 0
+    nums.push_back(7); // 1
+    nums.push_back(3); // 2
+    nums.push_back(6); // 3
+    nums.push_back(5); // 4
+    nums.push_back(6); // 5
+
+    // case 2 ans -1
+    // nums.push_back(1);
+    // nums.push_back(2);
+    // nums.push_back(3);
+
+    // case 3 ans 0
+    // nums.push_back(2);
+    // nums.push_back(1);
+    // nums.push_back(-1);
+
+    //Initialize leftmost and rightmost index to 0
+    vector<int> leftSum(nums.size(), 0);
+    vector<int> rightSum(nums.size(), 0);
+
+    // Calculate left Sum
+    for (int i = 1; i < nums.size(); ++i) {
+        leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        // cout << leftSum[i] << " " << leftSum[i - 1] << " " << nums[i - 1] << " "
+        //      << i << endl; // debugging
+    }
+    cout << endl;
+    // Calculate right Sum
+    for (int i = nums.size() - 2; i >= 0; --i) {
+        rightSum[i] = rightSum[i + 1] + nums[i + 1];
+        // cout << rightSum[i] << " " << rightSum[i + 1] << " " << nums[i + 1] << "
+        // "
+        //      << i << endl; //
+        // // debugging
+    }
+
+    // check
+    for (int i = 0; i < nums.size(); ++i) {
+        if (leftSum[i] == rightSum[i]) {
+        cout << "Pivot Index is: " << i << endl;
+        return;
+        }
+    }
+    cout << "Pivot Index is: " << -1 << endl;
+}
 
 int main(){
 
-    haskeyPairTwoSum();
+    // haskeyPairTwoSum();
+    findPivotIndex();
     // return 0;
 }
