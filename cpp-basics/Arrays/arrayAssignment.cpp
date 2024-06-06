cb548746b4ff8951e31a8bedbfbcc02e04b74e86
adityahongal <adityahongalwork@gmail.com>
2024-06-06 13:37:04 +0530

    Removing Duplicates

--- cpp-basics/Arrays/arrayAssignment.cpp
+++ cpp-basics/Arrays/arrayAssignment.cpp
@@ -7,6 +7,16 @@
 #include <vector>
 using namespace std;
 
+void printVector(vector<int> v) {
+  cout << "Printing Vector" << endl;
+  int size = v.size();
+  for (int i = 0; i < size; i++) {
+    // cout << v[i] << " "; // method 1
+    cout << v.at(i) << " ";
+  }
+  cout << endl;
+}
+
 // Finding Key pair/Two Sum problem
 // Time Complexity ---> O(nlogn)
 // Space Complexity ---> O(1)
@@ -163,12 +173,61 @@ void missingNumberByXORMethod(){
     cout << "The missing number using XOR Method is: " << ans << endl;
 }
 
+// Leetcode 26
+// Given an integer array nums sorted in non-decreasing order, remove the
+// duplicates in-place such that each unique element appears only once. The
+// relative order of the elements should be kept the same. Then return the
+// number of unique elements in nums. Consider the number of unique elements of
+// nums to be k, to get accepted, you need to do the following things: • Change
+// the array nums such that the first k elements of nums contain the unique
+// elements in the order they were present in nums initially. The remaining
+// elements of nums are not important as well as the size of nums:. • Return k.
+// Remove Duplicates
+// Input: nums = [0,0,1, 1, 1,2,2,3,3,4]
+// Output: 5, nums =
+// ［0,1,2,3,4._,_,_,_,_］
+// Explanation: Your function should return k = 5, with the
+// first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are
+// underscores).
+
+void removeDuplicates() {
+  vector<int> arr;
+  arr.push_back(0);
+  arr.push_back(0);
+  arr.push_back(1);
+  arr.push_back(1);
+  arr.push_back(1);
+  arr.push_back(2);
+  arr.push_back(2);
+  arr.push_back(3);
+  arr.push_back(3);
+  arr.push_back(4);
+
+  int i = 1, j = 0;
+
+  while (i < arr.size()) {
+    if (arr[i] == arr[j]) {
+      i++;
+    } else
+      arr[++j] = arr[i++];
+    // else {
+    //   j++;
+    //   arr[j] = arr[i];
+    //   i++;
+    // }
+  }
+  cout << "K is: " << j + 1 << endl;
+  printVector(arr);
+}
+
 int main(){
 
     // haskeyPairTwoSum();
     // FindPivotIndexBruteForce();
     // findPivotIndex();
     // missingNumberBySortMethod();
-    missingNumberByXORMethod();
+    // missingNumberByXORMethod();
+    removeDuplicates();
     // return 0;
 }
