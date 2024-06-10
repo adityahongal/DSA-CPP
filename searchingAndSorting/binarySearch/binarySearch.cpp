df6823015ffbac7cd1023022f9fd2076ee595316
adityahongal <adityahongalwork@gmail.com>
2024-06-10 12:34:35 +0530

    Finding First Occurence

--- searchingAndSorting/binarySearch/binarySearch.cpp
+++ searchingAndSorting/binarySearch/binarySearch.cpp
@@ -29,8 +29,37 @@ void basicBinarySearch(){
     cout << "Target not found" << endl;
 }
 
+int findFirstOccurance() {
+  int arr[8] = {10, 20, 20, 30, 30, 30, 40, 50};
+  int target = 20;
+  int start = 0;
+  int end = 7;
+  // int mid = (start + end) / 2;
+  int mid = start + (end - start) / 2; // Best Practice to avoid integer overflow (if sum of start and end is very big it will overflow)
+  int answer = -1;
+
+  while (start <= end) {
+    if (arr[mid] == target) {
+      answer = mid;
+      // move towards left
+      end = mid - 1;
+    } else if (target > arr[mid]) {
+      start = mid + 1;
+    } else if (target < arr[mid]) {
+      end = mid - 1;
+    }
+    //update mid
+    mid = (start + end) / 2;
+  }
+
+  cout << "First Occurance of target " << target << " is at: " << answer
+       << endl;
+  return answer;
+}
+
 int main(){
 
-    basicBinarySearch();
+    // basicBinarySearch();
+    findFirstOccurance();
     return 0;
 }
