2b892e6b4158c969178beed4c207219cc91ec1da
adityahongal <adityahongalwork@gmail.com>
2024-06-21 19:35:11 +0530

    ASCII values and custom delimiter

--- charsAndStrings/level1.cpp
+++ charsAndStrings/level1.cpp
@@ -6,17 +6,39 @@ void initchars(){
     //Creation of character array
     char arr[100];
 
-    //Taking input
+    // Taking input
+
     // Char array can take input all at once
-    //But in int array we need to give inputs one by one
-    //ending of char array will be a null character "\0"
-    //it acts as terminator for that array
-    
+    // But in int array we need to give inputs one by one
+    // ending of char array will be a null character "\0"
+    // it acts as terminator for that array
+    // Every characterhas its own ASCII value
+    // The ASCII value of null is 0
+
     cout << "Enter your name: " << endl;
-    cin >> arr;
+    // cin >> arr;
+
+    // Taking inputs with spaces or new line is not possible
+    // using cin
+    // so we need to use our custom delimiter to handle this case
+    // we use getline
+    // Example Sentence type input
+
+    cin.getline(arr,100);
+    // cin.getline(arr,100, '\t');        // Custom delimiter. Stops taking input when we hit tab
 
     //Printing character array
     cout << "Your name is: " << arr << endl;
+
+    // Printing ASCII value of each character
+    // We use typcasting to do that
+    // enter 4 chars to verify null value
+    
+    // cout << arr[0] << " --> " << (int)arr[0] << endl;
+    // cout << arr[1] << " --> " << (int)arr[1] << endl;
+    // cout << arr[2] << " --> " << (int)arr[2] << endl;
+    // cout << arr[3] << " --> " << (int)arr[3] << endl;
+    // cout << arr[4] << " --> " << (int)arr[4] << endl;
 }
 
 int main(){
