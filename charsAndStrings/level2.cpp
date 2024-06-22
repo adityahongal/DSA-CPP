64db8c8b0b1ba4ec32849c5ef1186b18046680e8
adityahongal <adityahongalwork@gmail.com>
2024-06-22 14:33:31 +0530

    remove All Occurances Of Substring

--- charsAndStrings/level2.cpp
+++ charsAndStrings/level2.cpp
@@ -50,8 +50,41 @@ void removeAdjacentDuplicateStrings(string str){
     cout << "Answer is: " << ans << endl;    
 }
 
+// Leetcode 1910. Remove All Occurrences of a Substring
+
+// Given two strings s and part, perform the following operation on s until all
+// occurrences of the substring part are removed:
+
+// Find the leftmost occurrence of the substring part and remove it from s.
+// Return s after removing all occurrences of part.
+
+// A substring is a contiguous sequence of characters in a string.
+
+// Example 1:
+
+// Input: s = "daabcbaabcbc", part = "abc"
+// Output: "dab"
+// Explanation: The following operations are done:
+// - s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
+// - s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
+// - s = "dababc", remove "abc" starting at index 3, so s = "dab".
+// Now s has no occurrences of "abc".
+
+void removeAllOccurancesOfSubstring(string str, string subString) {
+  while (str.find(subString) != string::npos) {
+    int startOfSubString = str.find(subString);
+    int subStringLength = subString.length();
+    str.erase(startOfSubString, subStringLength);
+  }
+
+  cout << "After removing duplicate substring: " << str << endl;
+}
+
 int main(){
-    removeAdjacentDuplicateStrings("abbaca");
-    removeAdjacentDuplicateStrings("azxxzy");
+    // removeAdjacentDuplicateStrings("abbaca");
+    // removeAdjacentDuplicateStrings("azxxzy");
+
+    removeAllOccurancesOfSubstring("daabcbaabcbc", "abc");
+    removeAllOccurancesOfSubstring("axxxxyyyyb", "xy");
     return 0;
 }
