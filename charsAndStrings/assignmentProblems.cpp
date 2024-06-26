9e17c1b2f92a9441e6e5d05552a2645b30833f7c
adityahongal <adityahongalwork@gmail.com>
2024-06-26 15:26:17 +0530

    Valid Anagram

--- charsAndStrings/assignmentProblems.cpp
+++ charsAndStrings/assignmentProblems.cpp
@@ -24,7 +24,45 @@ void print1DCharVector(vector< char > arr) {
   cout << endl;
 }
 
-int main(){
+// Leetcode 242 Valid Anagram
 
+void isAnagram(string str, string testString) {
+    // method 1 sort and compare
+    sort(str.begin(), str.end());
+    sort(testString.begin(), testString.end());
+
+    cout << (str == testString) << endl;
+}
+
+// method 2 using frequency table/hash table
+
+void isAnagramByFrequencyMethod(string str, string testString) {
+  int freqTable[256] = {0}; // ASCI range for all chars are 2^8 or 256 max
+
+  for (int i = 0; i < str.size(); i++) {
+    freqTable[str[i]]++;
+  }
+
+  for (int i = 0; i < testString.size(); i++) {
+    freqTable[testString[i]]--;
+  }
+
+  for (int i = 0; i < 256; i++) {
+    if (freqTable[i] != 0) {
+      cout << "Not Anagram" << endl;
+      return;
+    }
+  }
+
+  cout << "It is an Anagram" << endl;
+  return;
+} // TC = O(m) + O(n) + O(256) = O(m+n)
+
+int main(){
+    // isAnagram("anagram", "nagaram");
+    // isAnagram("rat", "car");
+
+    isAnagramByFrequencyMethod("anagram", "nagaram");
+    isAnagramByFrequencyMethod("rat", "car");
     return 0;
 }
