4657540541bdf46fddaa218e2329b743b7311145
adityahongal <adityahongalwork@gmail.com>
2024-07-01 20:47:53 +0530

    Counting Primes SQRT method

--- mathsAndPointers/primeNumbers.cpp
+++ mathsAndPointers/primeNumbers.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include <cmath>              // for square root function
 using namespace std;
 
 // Count Primes leetcode - 204
@@ -26,8 +27,35 @@ int countPrimesNaive(int n) {
     return c;
 }
 
+// Using square root
+// TC O(sqrt(n)) + O(n) = O(n sqrt(n))
+// this solution too exceeds time limit on leetcode
+bool isPrimeSQRT(int n) {
+    if(n <= 1) return false;
+
+    int sqrtN = sqrt(n);
+    for(int i= 2; i<=sqrtN ;i++) {
+        if(n % i == 0) {
+        return false;
+        }
+    }
+    return true;
+}
+
+int countPrimesSQRT(int n) {
+    int c = 0;
+    for(int i=0; i< n; i++) {
+        if(isPrimeSQRT(i)) {
+        ++c;
+        }
+    }
+    return c;
+}
+
 int main(){
 
-    cout << "Naive Approach: " << countPrimesNaive(10) << endl;    
+    cout << "Naive Approach: " << countPrimesNaive(10) << endl; 
+    cout << "SQRT Approach: " << countPrimesSQRT(21) << endl;  
+    
     return 0;
 }
