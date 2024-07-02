178b83d15680ee48fa4df88a06ed5ce2395c5cb1
adityahongal <adityahongalwork@gmail.com>
2024-07-02 20:22:12 +0530

    Accessing Pointers with datatypes

--- mathsAndPointers/pointersLevel1.cpp
+++ mathsAndPointers/pointersLevel1.cpp
@@ -11,6 +11,25 @@ void pointersInit() {
     // pointer create
     // To store address we need a pointer variable
     int *ptr = &a;
+
+    // acccessing pointers
+    cout << "Accessing Pointer itself: " << ptr << endl;
+    cout << "Accessing Pointer Value: " << *ptr << endl;
+    cout << "Accessing Address of Pointer: " << &ptr << endl;
+    cout << "Accessiing Size of int pointer: " << sizeof(ptr) << endl;
+
+    // pointer with different datatypes
+    char ch = 'k';
+    char *chptr = &ch;
+    cout << "Accessiing Size of char pointer: " << sizeof(chptr) << endl;
+
+    long num = 10;
+    long *lptr = &num;
+    cout << "Accessiing Size of char pointer: " << sizeof(lptr) << endl;
+
+    // the answer of size of different datatype pointers are same why ?
+    // because ptr variable stores only address
+    // so answer for 32 bit system might be 4 and for 64 bit will be 8
 }
 
 int main(){
