3ff3357ce41cae11e8fb72c0c82620504512114c
adityahongal <adityahongalwork@gmail.com>
2024-07-08 21:49:08 +0530

    Search array using recursion

--- recursion/level2.cpp
+++ recursion/level2.cpp
@@ -20,11 +20,29 @@ void printArrayRecursive(int arr[], int size, int index){
     printArrayRecursive(arr, size, index + 1);
 }
 
+// Searching array using recursion
+bool searchArray(int arr[], int size, int target, int index) {
+    if (index >= size) {                     // Target not found
+        return false;
+    }
+
+    if (arr[index] == target) {             // Target found
+        return true;
+    }
+
+    bool ans = searchArray(arr, size, target, index + 1);  // Recursive call
+    return ans;
+}
+
 int main(){
     // Print array using recursion
     int arr[] = {10,20,30,40,50};
     int sizeOfArr = sizeof(arr) / sizeof(arr[0]);
     int index = 0;
-    printArrayRecursive(arr, sizeOfArr, index);
+    // printArrayRecursive(arr, sizeOfArr, index);
+
+    // Search array using recursion
+    int target = 50;
+    cout << searchArray(arr,sizeOfArr,target,index) << endl;
     return 0;
 }
