7e11fec228355d26ce202b6a5f79769aaf4413b7
adityahongal <adityahongalwork@gmail.com>
2024-07-08 14:52:30 +0530

    Head and Tail Recursion

--- recursion/level1.cpp
+++ recursion/level1.cpp
@@ -16,7 +16,43 @@ int factorial(int n) {
     // Processing - optional
 }
 
+// Print Count N --> 1
+// when recursive call is at bottom of function
+// It is called Tail Recursive call
+void printReverseCount(int n) {
+    // base case
+    if (n == 0 || n == 1) {
+        cout << 1 << endl;
+        return;
+    }
+
+    // processing
+    cout << n << " ";
+
+    // recursive call
+    printReverseCount(n - 1);        // Tail Recursion
+}
+
+// Print Count 1 --> N
+// when recursive call is not at bottom of function
+// It is called Head Recursive call
+void printCount(int n) {
+    // base case
+    if (n == 0 || n == 1) {
+        cout << 1 << endl;
+        return;
+    }
+
+    // recursive call
+    printCount(n - 1);        // Head Recursion
+
+    // processing
+    cout << n << " ";
+}
+
 int main(){
-    cout << factorial(5) << endl;
+    // cout << factorial(5) << endl;
+    printReverseCount(10);
+    printCount(10);
     return 0;
 }
