bc61c7253b2dc60339f0f00faae805e0d55e33f3
adityahongal <adityahongalwork@gmail.com>
2024-07-09 13:43:44 +0530

    Print even numbers using recursion

--- recursion/level2.cpp
+++ recursion/level2.cpp
@@ -43,7 +43,7 @@ void maxNumInArr(int arr[], int size, int index,int &maxNum) { // v impo &maxNum
 
     // processing
     maxNum = max(maxNum, arr[index]);
-    
+
     // recursive call
     maxNumInArr(arr, size, index + 1, maxNum);
 }
@@ -62,9 +62,33 @@ void minNumInArr(int arr[], int size, int index,int &minNum) { // v impo &minNum
     minNumInArr(arr, size, index + 1, minNum);
 }
 
+void printVector(vector< int > v) {
+    cout << "Printing Vector" << endl;
+    int size = v.size();
+    for (int i = 0; i < size; i++) {
+        // cout << v[i] << " "; // method 1
+        cout << v.at(i) << endl;
+    }
+    cout << endl;
+}
+
+// Print ans store even number of elements
+void evenNumsInArray(int arr[], int size, int index, vector< int > &ans) {
+    if (index >= size) {
+        return;
+    }
+
+    if (arr[index] % 2 == 0) {
+        ans.push_back(arr[index]);
+    }
+
+    evenNumsInArray(arr, size, index + 1, ans);
+}
+
 int main(){
     // Print array using recursion
-    int arr[] = {10,20,30,40,50};
+    // int arr[] = {10,20,30,40,50};
+    int arr[] = {10,11,12,13,14,15,16,17};
     int sizeOfArr = sizeof(arr) / sizeof(arr[0]);
     int index = 0;
     // printArrayRecursive(arr, sizeOfArr, index);
@@ -74,14 +98,19 @@ int main(){
     // cout << searchArray(arr,sizeOfArr,target,index) << endl;
 
     // Max number in an array using recursion
-    int maxNum = INT_MIN;
-    maxNumInArr(arr, sizeOfArr, index, maxNum);
-    cout << maxNum << endl;
+    // int maxNum = INT_MIN;
+    // maxNumInArr(arr, sizeOfArr, index, maxNum);
+    // cout << maxNum << endl;
 
-    // Min number in an array using recursion
-    int minNum = INT_MAX;
-    minNumInArr(arr, sizeOfArr, index, minNum);
-    cout << minNum << endl;
+    // // Min number in an array using recursion
+    // int minNum = INT_MAX;
+    // minNumInArr(arr, sizeOfArr, index, minNum);
+    // cout << minNum << endl;
+
+    // Print and Store even numbers in an array using recursion
+    vector<int> ans;
+    evenNumsInArray(arr, sizeOfArr, index, ans);
+    printVector(ans);
 
     return 0;
 }
