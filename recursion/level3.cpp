f4dfdd073bc82fb999e2add1848523b85fb12cbd
adityahongal <adityahongalwork@gmail.com>
2024-07-11 15:02:08 +0530

    Include Exclude Pattern in recursion

--- recursion/level3.cpp
+++ recursion/level3.cpp
@@ -23,14 +23,40 @@ bool checkSorted(int arr[], int size, int index){
 
 }
 
+// Include Exclude Pattern ! ! ! ! ! ! ! !
+void printSubsequences(string str, int i, string output,vector<string> &ans) {
+    //base case
+    if(i == str.length()) {
+        // cout << output << endl;
+        ans.push_back(output);
+        return;
+    }
+
+    char ch = str[i];
+    //include ch 
+    printSubsequences(str,i+1,output + ch,ans);
+    //exclude ch
+    printSubsequences(str,i+1,output,ans);
+}
+
 int main(){
 
-    int arr[] = {10,20,30,40,50};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int index = 0;
-    bool ans = checkSorted(arr,size, index);
-    cout << "Array is sorted or not: " << ans << endl;
+    // int arr[] = {10,20,30,40,50};
+    // int size = sizeof(arr) / sizeof(arr[0]);
+    // int index = 0;
+    // bool ans = checkSorted(arr,size, index);
+    // cout << "Array is sorted or not: " << ans << endl;
 
+    // Include Exclude Pattern ! ! ! ! ! ! ! !
+    string str = "abc";
+    string output = "";
+    int index=0;
 
+    vector<string> ans;
+    printSubsequences(str,index,output,ans);
+    for(auto index: ans) {
+    cout << index << endl;
+    }
+    
     return 0;
 }
