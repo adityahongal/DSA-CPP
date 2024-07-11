d7cf590af5591df566a963234618fe45b4bb8f95
adityahongal <adityahongalwork@gmail.com>
2024-07-11 19:49:56 +0530

    House Robber Recursion

--- recursion/level3.cpp
+++ recursion/level3.cpp
@@ -39,6 +39,24 @@ void printSubsequences(string str, int i, string output,vector<string> &ans) {
     printSubsequences(str,i+1,output,ans);
 }
 
+// Leetcode 198. House Robber
+int rob(vector<int> &nums, int size, int index) {
+    if(index >= size) {
+        return 0;
+    }
+
+    // rob -> ith index don't rob -> i+1 th index
+    // Include ith index
+    int includeAns = nums[index] + rob(nums, size, index + 2);
+
+    // Exclude ith index
+    // do not rob -> ith index  rob -> i+1 index
+    int excludeAns = 0 + rob(nums, size, index + 1);
+
+    int finalAns = max(includeAns, excludeAns);
+    return finalAns;
+}
+
 int main(){
 
     // int arr[] = {10,20,30,40,50};
@@ -48,15 +66,33 @@ int main(){
     // cout << "Array is sorted or not: " << ans << endl;
 
     // Include Exclude Pattern ! ! ! ! ! ! ! !
-    string str = "abc";
-    string output = "";
-    int index=0;
+    // string str = "abc";
+    // string output = "";
+    // int index=0;
 
-    vector<string> ans;
-    printSubsequences(str,index,output,ans);
-    for(auto index: ans) {
-    cout << index << endl;
-    }
+    // vector<string> ans;
+    // printSubsequences(str,index,output,ans);
+    // for(auto index: ans) {
+    // cout << index << endl;
+    // }
+
+    // House Rob
+    vector<int> robOne;
+    vector<int> robTwo;
+
+    robOne.push_back(1);
+    robOne.push_back(2);
+    robOne.push_back(3);
+    robOne.push_back(1);
+
+    robTwo.push_back(2);
+    robTwo.push_back(7);
+    robTwo.push_back(9);
+    robTwo.push_back(3);
+    robTwo.push_back(1);
+
+    cout << rob(robOne, robOne.size(), 0) << endl;
+    cout << rob(robTwo, robTwo.size(), 0) << endl;
     
     return 0;
 }
