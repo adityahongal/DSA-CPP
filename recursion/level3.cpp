7c15e4e20c79f1d28812c916aeadbe947bda8198
adityahongal <adityahongalwork@gmail.com>
2024-07-11 21:17:09 +0530

    Leetcode 322 Coin Change Recursion

--- recursion/level3.cpp
+++ recursion/level3.cpp
@@ -57,6 +57,68 @@ int rob(vector<int> &nums, int size, int index) {
     return finalAns;
 }
 
+// Leetcode 322 Coin Change
+// You are given an integer array coins representing coins of different
+// denominations and an integer amount representing a total amount of money.
+
+// Return the fewest number of coins that you need to make up that amount.
+// !!!!!!!
+
+// If that amount of money cannot be made up by any combination of the coins,
+// return -1. You may assume that you have an infinite number of each kind of
+// coin.
+
+// Example 1:
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+
+int solveCoinChange(vector< int > &coins, int amount) {
+
+    if (amount == 0) {
+        return 0;                   //0 amount create krne k liye kitne coins - base case
+    }
+    // initialize minimum coins
+    //if amount < 0
+    int minCoinAns = INT_MAX;
+
+    //hr amount k liye poore coins k array ko
+    //traverse krre h 
+        for(int i=0; i<coins.size(); i++) {
+            //coin value
+            int coin = coins[i];
+
+            //if coin value > amount value, no need to call recursive function
+            //if coin value <= amount value , call recursive fiunction
+            if( coin <= amount) {
+                //1 coin toh use hogya, amount becomes amount-coins
+                //baaki reciursion sambhal lega
+                int recursionAns = solveCoinChange(coins, amount-coin);
+                //ho skta h recursionAns valid ho ya fer Invalid 
+                //invalid case -> recursionAns -> INT_MAX;
+                //valid case -> recursionAns -> not equal to INT_MAX;
+                if(recursionAns != INT_MAX) {
+                    //valid answer aaaya h 
+                    int coinsUsed = 1 + recursionAns;
+                    //kya above answer minimum hai
+                    minCoinAns = min(minCoinAns, coinsUsed);
+                }
+            }
+        }
+
+  return minCoinAns;
+}
+int coinchange(vector< int > &coins, int amount) {
+    int ans = solveCoinChange(coins, amount);
+
+    if (ans == INT_MAX) {
+        return -1;
+    }
+    else{
+        return ans;
+    }
+}
+
 int main(){
 
     // int arr[] = {10,20,30,40,50};
@@ -77,22 +139,38 @@ int main(){
     // }
 
     // House Rob
-    vector<int> robOne;
-    vector<int> robTwo;
+    // vector<int> robOne;
+    // vector<int> robTwo;
 
-    robOne.push_back(1);
-    robOne.push_back(2);
-    robOne.push_back(3);
-    robOne.push_back(1);
+    // robOne.push_back(1);
+    // robOne.push_back(2);
+    // robOne.push_back(3);
+    // robOne.push_back(1);
 
-    robTwo.push_back(2);
-    robTwo.push_back(7);
-    robTwo.push_back(9);
-    robTwo.push_back(3);
-    robTwo.push_back(1);
+    // robTwo.push_back(2);
+    // robTwo.push_back(7);
+    // robTwo.push_back(9);
+    // robTwo.push_back(3);
+    // robTwo.push_back(1);
 
-    cout << rob(robOne, robOne.size(), 0) << endl;
-    cout << rob(robTwo, robTwo.size(), 0) << endl;
+    // cout << rob(robOne, robOne.size(), 0) << endl;
+    // cout << rob(robTwo, robTwo.size(), 0) << endl;
+
+    // Coin change
+    vector< int > coinChangeCaseOne;
+    int amountOne = 11;
+    
+    coinChangeCaseOne.push_back(1);
+    coinChangeCaseOne.push_back(2);
+    coinChangeCaseOne.push_back(5);
+
+    vector< int > coinChangeCaseTwo;
+    int amountTwo = 3;
+
+    coinChangeCaseTwo.push_back(2);
+
+    cout << coinchange(coinChangeCaseOne, amountOne) << endl;
+    cout << coinchange(coinChangeCaseTwo, amountTwo) << endl;
     
     return 0;
 }
