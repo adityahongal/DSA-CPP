533c4ee3d73a3698436b7fb01acdf2a8bb2ba73a
adityahongal <adityahongalwork@gmail.com>
2024-07-18 15:22:48 +0530

    find Last occurance of a char Recursively

--- recursion/assignmentRecursion.cpp
+++ recursion/assignmentRecursion.cpp
@@ -18,15 +18,38 @@ void findLastOccuranceOfChar(string str, int index, int &ans, char target) {
     findLastOccuranceOfChar(str, index + 1, ans, target);     // recursive call
 }
 
+// Method 2 - Search from Right to left recursively
+void findLastOccuranceOfCharRTL(string str, int index, int &ans, char target) {
+    if (str[index] < 0 ) {                    // Base Case
+        return;
+    }
+
+    if (str[index] == target) {
+        ans = index;
+        return;
+    }
+
+    findLastOccuranceOfCharRTL(str, index - 1, ans, target);     // recursive call // i = n-1
+}
+
 int main(){
 
     // for last occurance of char
-    int index = 0;
-    int ans = -1;
-    findLastOccuranceOfChar("abcddedg", index, ans, 'd');
-    cout << "Found on index: " << ans << endl;
+    // Method 1
+    // int index = 0;
+    // int ans = -1;
+    // findLastOccuranceOfChar("abcddedg", index, ans, 'd');
+    // cout << "Found on index: " << ans << endl;
 
     // last occr using inbuilt STL function
     const char* last_occurrence = strrchr("abcddedg", 'd');
     cout << "using inbuilt strrchr(): " << last_occurrence << endl;
+
+    // for last occurance of char
+    // Method 2
+    string str = "abcddedg";
+    int index = str.size() - 1;  // start from rightmost index (n-1)
+    int ans = -1;
+    findLastOccuranceOfCharRTL(str, index, ans, 'd');
+    cout << "Found on index using RTL: " << ans << endl;
 }
