0311cc08d5a58cdfc5174b8791397f2c8a65d696
adityahongal <adityahongalwork@gmail.com>
2024-07-19 22:12:58 +0530

    Add strings recursively leetcode-415

--- recursion/assignmentRecursion.cpp
+++ recursion/assignmentRecursion.cpp
@@ -62,6 +62,42 @@ bool checkPalindrome(string &str, int start, int end) {
     return checkPalindrome(str, start + 1, end - 1);
 }
 
+// Leetcode 415. Add Strings
+// N = max(num1, num2)
+// m = constant
+// TC O(N + 1) -> O(N)
+// SC O(m(N+1)) -> O(N)
+// take by ref(&num1,&num2) parameters to reduce space complexity
+void addStrings(string &num1, int ptr1, string &num2, int ptr2, string &ans,
+                int carry = 0) {
+    
+    if (ptr1 && ptr2 < 0) {
+        // extra carry might be remaining
+        if (carry != 0) {
+        // return string(1, carry + '0');
+        ans.push_back(carry + '0');
+        }
+        // return "";
+        return;
+    }
+
+    // ex sum ptr1 is at 4 in 456 and sum2 56 -> ptr2 will be at diff positions
+    
+    // string ans = "";
+
+    int digitOfNum1 = (ptr1 >= 0 ? num1[ptr1] : '0') - '0';
+    int digitOfNum2 = (ptr2 >= 0 ? num2[ptr2] : '0') - '0';
+    int csum = digitOfNum1 + digitOfNum2 + carry;
+
+    int remainingDigit = csum % 10;
+    carry = csum / 10;
+    ans.push_back(remainingDigit + '0');
+
+    // ans += addStrings(num1, ptr1 - 1, num2, ptr2 - 1, ans, carry);
+    addStrings(num1, ptr1 - 1, num2, ptr2 - 1, ans, carry);
+
+    // return ans;
+}
 
 int main(){
 
@@ -91,10 +127,23 @@ int main(){
     // reverseString(str, start, end);
     // cout << str << endl;
 
-    // Check palindrome
-    string str1 = "racecar";
-    cout << checkPalindrome(str1, 0, str1.size() - 1) << endl;
+    // // Check palindrome
+    // string str1 = "racecar";
+    // cout << checkPalindrome(str1, 0, str1.size() - 1) << endl;
 
-    string str2 = "racetar";
-    cout << checkPalindrome(str2, 0, str2.size() - 1) << endl;
+    // string str2 = "racetar";
+    // cout << checkPalindrome(str2, 0, str2.size() - 1) << endl;
+
+    // for string add
+    string num1 = "456", num2 = "77";
+    string ans1 = "";
+    addStrings(num1, num1.size() - 1, num2, num2.size() - 1, ans1);
+    reverse(ans1.begin(), ans1.end());
+    cout << ans1 << endl;
+
+    string num3 = "11", num4 = "77";
+    string ans2 = "";
+    addStrings(num3, num3.size() - 1, num4, num4.size() - 1, ans2);
+    reverse(ans2.begin(), ans2.end());
+    cout << ans2 << endl;
 }
